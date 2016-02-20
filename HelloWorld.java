0f43d08b88c9208fca75369835d7066677ba1afe
SAC-CS112-Ricardo-Martinez <ric_martinez@icloud.com>
2016-02-20 08:11:22 -0800

    Added files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Ricardo Martinez
+ * CMPR 112 HW 1
+ * Hello World Program
+ */
+
+/**
+ *
+ * @author Ricardo Martinez
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
