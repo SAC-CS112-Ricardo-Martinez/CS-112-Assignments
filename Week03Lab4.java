4c5de5db728eb3691235b16c730657bad589d250
SAC-CS112-Ricardo-Martinez <ric_martinez@icloud.com>
2016-02-24 21:14:20 -0800

    Added files via upload

---
+++ Week03Lab4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week03lab4;
+
+/**
+ *
+ * @author Dora72
+ */
+public class Week03Lab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String greeting = "Hello"+" World!";
+        System.out.printf("The greeting is %s ", greeting);
+    }
+    
+}
