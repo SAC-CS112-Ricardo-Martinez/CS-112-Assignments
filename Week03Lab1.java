038bb5f5bc2402ba7c7ef16e050269dedc8e9b9d
SAC-CS112-Ricardo-Martinez <ric_martinez@icloud.com>
2016-02-24 21:06:07 -0800

    Added files via upload

---
+++ Week03Lab1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mynameislab1;
+
+/**
+ *
+ * @author Dora72
+ */
+public class MyNameIsLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        System.out.println("My name is Ricardo Martinez");
+    }
+    
+}
---
+++ Week03Lab2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mynameislab2;
+
+/**
+ *
+ * @author Dora72
+ */
+public class MyNameIsLab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello\nMy Name is Ricardo Martinez");
+    }
+    
+}
