fadf6d9535ef44a5a105cee8998cfe1d9d20e7cd
SAC-CS112-Ricardo-Martinez <ric_martinez@icloud.com>
2016-03-16 20:12:52 -0700

    Added files via upload

---
+++ Week06Lab2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week06lab2;
+import java.util.Scanner;
+public class Week06Lab2 {
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) {
+    // TODO code application logic here 
+    int personNumber; //Person enters a number from 1 to 10  
+    int computerNumber; //Randomly generated number used to determine 
+                     //computer's number
+    Scanner scan = new Scanner(System.in);
+    computerNumber = 1 + (int)(Math.random() * 10);
+    System.out.println("Please type an integer from 1 to 10.");
+    personNumber = scan.nextInt();
+    if (computerNumber == personNumber)
+        System.out.println("You chose wisely.");
+    else{
+        if (computerNumber > personNumber)
+            System.out.println("You chose low.");
+        else System.out.println("You chose high.");
+        }
+    System.out.printf("The computer number is %d%n" ,computerNumber);
+    }
+}
